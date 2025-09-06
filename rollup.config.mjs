1f6bc577dfb945f549e87929f24324b8c83eb9ea
chlbri <51156116+chlbri@users.noreply.github.com>
2025-09-06 19:07:12 +0000

    feat: Add auto-generated types and constants for functions, numbers, objects, strings, promises, and transformations
    - Introduced `Fn`, `FnBasic`, and `Checker` types in functions/types.ts.
    - Added `DIGITS` constant in numbers/constants.ts and corresponding types in numbers/types.ts.
    - Created `TrueObject`, `To`, `NOmit`, `DeepOmit`, and various utility types in objects/types.ts.
    - Implemented `Picker` type in objects/utils/types.ts.
    - Developed `PromisifyMethod` and `PromisifyObject` types in promises/types.ts.
    - Defined `ENGLISH_LETTERS` and `DEFAULT_DELIMITER` constants in strings/constants.ts, along with related types in strings/types.ts.
    - Established `PRIMITIVES`, `PRIMITIVE_OBJECTS`, `CUSTOM`, and `PARTIAL` constants in transform/constants.ts, and created related types in transform/types.ts.
    - Consolidated all feature types into globals/types.ts for easier access.
    - Added `_unknown` utility function in globals/utils/_unknown.ts for type assertion.

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,4 +3,12 @@ import { defineConfig } from '@bemedev/rollup-config';
 export default defineConfig.bemedev({
   declarationMap: true,
   ignoresJS: '**/*.example.ts',
+  plugins: [
+    'tsPaths',
+    'alias',
+    'typescript',
+    'circulars',
+    'externals',
+    'clean',
+  ],
 });
